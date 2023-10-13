bd56e8b2e58de885176e960286276fce369d7919
reindert <info@codesensei.nl>
2023-10-13 15:05:17 +0200

    reorganization
    - colours and faces in their own file
    - allow better customization
    - update faces immediately after customization changes
    - drop support for terminals with less than 256 colours
    - better support for org mode
    - better support for info pages
    - 3d buttons

--- fnord-colours.el
+++ fnord-colours.el
@@ -18,40 +18,29 @@
 ;;; Code:
 
 (defgroup fnord-theme nil
-  "Nord theme customizations.
+  "Fnord theme customizations.
 The theme has to be reloaded after changing anything in this group."
   :group 'faces)
 
-(defcustom fnord-region-highlight nil
-  "Allows to set a region highlight style based on the Nord components.
-Valid styles are
-    - 'snowstorm' - Uses 'nord0' as foreground- and 'nord4' as background color
-    - 'frost' - Uses 'nord0' as foreground- and 'nord8' as background color"
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-uniform-mode-lines nil
-  "Enables uniform activate- and inactive mode lines using 'nord3' as background."
-  :type 'boolean
-  :group 'fnord-theme)
+(defgroup fnord-theme-colours nil
+  "Colour values for the fnord theme."
+  :group 'fnord-theme
+  :link '(url-link "https://www.nordtheme.com/docs/colors-and-palettes"))
 
 (defcustom fnord-comment-colour "#616e88"
-  "A special colour for comments that is not in the original Nord palette."
+  "Fnord theme colour 16 - A special colour for comments.
+This colour is not in the original Nord palette."
   :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-comment-colour-256 "darkgrey"
-  "A special colour for comments that is not in the original Nord palette."
-  :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-0 "#2E3440"
-  "Nord theme colour 0 - Polar Night. For background and area coloring."
+  "Fnord theme colour 0 - Polar Night (dark()). For background and area coloring."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-1 "#3B4252"
-  "Nord theme colour 1 - a brighter shade based on `fnord-0'.
+(defcustom fnord-01 "#3B4252"
+  "Fnord theme colour 1 - Polar night 1 (dark).
+A brighter shade based on `fnord-0'.
 For elevated, more prominent or focused UI elements like
 
 - status bars and text editor gutters
@@ -62,65 +51,71 @@ For elevated, more prominent or focused UI elements like
 It also works fine for more inconspicuous and passive elements like borders
  or as dropshadow between different components."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-2 "#434C5E"
-  "Nord theme colour 2 - an even brighter shade of `fnord-0'.
+(defcustom fnord-02 "#434C5E"
+  "Fnord theme colour 2 - Polar night 2 (dark).
 Used to colorize the currently active text editor line as well as selection-
 and text highlighting color. It can also be used as an brighter variant for
-the same target elements like `fnord-1'."
+the same target elements like `fnord-01'."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-3 "#4C566A"
-  "Nord theme colour 3 - the brightest shade of `fnord-0'.
+(defcustom fnord-03 "#4C566A"
+  "Fnord theme colour 3 - Polar night 3 (dark).
 For UI elements like indent- and wrap guide marker. In the context of code
-syntax highlighting it is used for comments and invisible/non-printable characters."
+syntax highlighting it is used for comments and invisible/non-printable
+ characters."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-4 "#D8DEE9"
-  "Nord theme colour 4 - Snow Storm.
+(defcustom fnord-04 "#D8DEE9"
+  "Fnord theme colour 4 - Snow Storm 1 (light).
 For UI elements like the text editor caret.
 In the context of syntax highlighting it is used as text color for variables,
 constants, attributes and fields."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-5 "#E5E9F0"
-  "Nord theme colour 5 - a brighter shade of `fnord-4'.
+(defcustom fnord-05 "#E5E9F0"
+  "Fnord theme colour 5 - Snow Storm 2 (light).
 Used for more subtle/inconspicuous UI text elements that do not need so much
 visual attention. Other use cases are also state animations like a more brighter
 text color when a button is hovered, active or focused."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-6 "#ECEFF4"
-  "Nord theme colour 6 - the brightest shade of `fnord-4'.
+(defcustom fnord-06 "#ECEFF4"
+  "Fnord theme colour 6 - Snow Storm 3 (light).
 For elevated UI text elements that require more visual attention.
 In the context of syntax highlighting it is used as text color for plain text as
-well as reserved and structuring syntax characters like curly- and square brackets."
+well as reserved and structuring syntax characters like curly- and square
+brackets."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-7 "#8FBCBB"
-  "Nord theme colour 7 - Frost: A calm and highly contrasted color.
+(defcustom fnord-07 "#8FBCBB"
+  "Fnord theme colour 7 - Frost 1 (green).
+A calm and highly contrasted color.
 Used for UI elements that should, next to the primary accent color nord8, stand
 out and get more visual attention.
-In the context of syntax highlighting it is used for classes, types and primitives."
+In the context of syntax highlighting it is used for classes, types and
+primitives."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-8 "#88C0D0"
-  "Nord theme colour 8 - bright and shiny primary accent color.
+(defcustom fnord-08 "#88C0D0"
+  "Fnord theme colour 8 - Frost 2 (light blue)
+Bright and shiny primary accent color.
 Used for primary UI elements with main usage purposes that require the most
 visual attention. In the context of syntax highlighting it is used for
 declarations, calls and execution statements of functions, methods and routines."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-9 "#81A1C1"
-  "Nord theme colour 9 - A more darkened and less saturated color.
+(defcustom fnord-09 "#81A1C1"
+  "Fnord theme colour 9 - Frost 3 (blue).
+A more darkened and less saturated color.
 Used for secondary UI elements that also require more visual attention than
 other elements.In the context of syntax highlighting it is used for language
 specific, syntactic and reserved keywords as well as
@@ -131,132 +126,70 @@ specific, syntactic and reserved keywords as well as
 - units
 - punctuations like (semi)colons, points and commas."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-10 "#5E81AC"
-  "Nord theme colour 10 - A dark and intensive color.
+  "Fnord theme colour 10 - Frost 4 (blue).
+A dark and intensive color.
 Used for tertiary UI elements that require more visual attention than default
 elements. In the context of syntax highlighting it is used for pragmas, comment
 keywords and pre-processor statements."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-11 "#BF616A"
-  "Nord theme colour 11 - Aurora.
+  "Fnord theme colour 11 - Aurora 1 (red).
 Used for UI elements that are rendering error states like linter markers and the
 highlighting of Git diff deletions. In the context of syntax highlighting it is
-used to override the highlighting of syntax elements that are detected as errors."
+used to override the highlighting of syntax elements that are detected as
+errors."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-12 "#D08770"
-  "Nord theme colour 12.
+  "Fnord theme colour 12 - Aurora 2 (orange).
 Rarely used for UI elements, but it may indicate a more advanced or dangerous
 functionality. In the context of syntax highlighting it is used for special
 syntax elements like annotations and decorators."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-13 "#EBCB8B"
-  "Nord theme colour 13.
+  "Fnord theme colour 13 - Aurora 3 (yellow).
 Used for UI elements that are rendering warning states like linter markers and
 the highlighting of Git diff modifications. In the context of syntax
 highlighting it is used to override the highlighting of syntax elements that are
-detected as warnings as well as escape characters and within regular expressions."
+detected as warnings as well as escape characters and within regular
+expressions."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-14 "#A3BE8C"
-  "Nord theme colour 14.
+  "Fnord theme colour 14 - Aurora 4 (light green).
 Used for UI elements that are rendering success states and visualizations and
 the highlighting of Git diff additions. In the context of syntax highlighting
 it is used as main color for strings of any type like double/single quoted or
 interpolated."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
 (defcustom fnord-15 "#B48EAD"
-  "Nord theme colour 15.
+  "Fnord theme colour 15 - Aurora 5 (purple).
 Rarely used for UI elements, but it may indicate a more uncommon functionality.
 In the context of syntax highlighting it is used as main color for numbers of
 any type like integers and floating point numbers."
   :type 'string
-  :group 'fnord-theme)
+  :group 'fnord-theme-colours)
 
-(defcustom fnord-1-256 "black"
-  "Nord theme colour 1 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
+(require 'cl-lib)
+(defun fnord--get-colour (n)
+  "Retrieve nord colour N. Colour 16 is the special comment colour."
+  (cl-assert (and (>= n 0) (< n 17)))
+  (when n
+    (nth n
+         (list fnord-0 fnord-01 fnord-02 fnord-03 fnord-04 fnord-05 fnord-06 fnord-07 fnord-08 fnord-09 fnord-10 fnord-11 fnord-12 fnord-13 fnord-14 fnord-15 fnord-comment-colour))))
 
-(defcustom fnord-2-256 "#434C5E"
-  "Nord theme colour 2 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
 
-(defcustom fnord-3-256 "brightblack"
-  "Nord theme colour 3 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-4-256 "#D8DEE9"
-  "Nord theme colour 4 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-5-256 "white"
-  "Nord theme colour 5 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-6-256 "brightwhite"
-  "Nord theme colour 6 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-7-256 "cyan"
-  "Nord theme colour 7 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-8-256 "brightcyan"
-  "Nord theme colour 8 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-9-256 "blue"
-  "Nord theme colour 9 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-10-256 "brightblue"
-  "Nord theme colour 10 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-11-256 "red"
-  "Nord theme colour 11 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-12-256 "brightyellow"
-  "Nord theme colour 12 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-13-256 "yellow"
-  "Nord theme colour 13 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-14-256 "green"
-  "Nord theme colour 14 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
-
-(defcustom fnord-15-256 "magenta"
-  "Nord theme colour 15 for terminals that do not support 256 colours."
-  :type 'string
-  :group 'fnord-theme)
 
 (provide 'fnord-colours)
 
---
+++ fnord-core.el
@@ -0,0 +1,344 @@
+;;; fnord-core.el --- Core faces for the fnord theme
+
+;; Copyright for portions of project fnord-theme are held by: Sven Greb
+;; <development@svengreb.de> (https://www.svengreb.de) 2016-2023, as part
+;; of project nordtheme/emacs (https://github.com/nordtheme/emacs). All
+;; other copyright for project fnord-theme are held by Reindert-Jan Ekker
+;; <info@rjekker.nl>, 2023-present.
+
+;; Title: Fnord Theme
+;; Project: fnord-theme
+;; URL: https://github.com/rjekker/fnord-theme
+;; Author: Reindert-Jan Ekker
+;; Package-Requires: ((emacs "24"))
+;; License: MIT
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'fnord-colours)
+
+
+(defface fnord--loaded-dummy
+  '((t (:weight normal)))
+  "A dummy face we can use to check whether fnord theme was loaded")
+
+
+(defun fnord--theme-loaded-p ()
+  (eq 'bold
+      (face-attribute 'fnord--loaded-dummy :weight )))
+
+
+(defun fnord--change-face (face attr value)
+  "Change a face attribute. Can pass ints for nord colourd. Nice for testing."
+    (set-face-attribute face nil
+                        attr
+                        (if (numberp value)
+                            (fnord--get-colour value)
+                          value)))
+
+
+(defun fnord--update-custom-face (symbol value face attr)
+  (when (fnord--theme-loaded-p)
+    (fnord--change-face face attr value))
+  (set-default-toplevel-value symbol value))
+
+
+
+(defcustom fnord-region-highlight-foreground nil
+  "Foreground color for the region.
+If set as an int: use a fnord theme colour.
+Alternatively, use a string to choose your own colour code. Note that the string
+has to be a valid colour code like \"#A01A02\""
+  :type '(choice (integer :tag "fnord theme colour" :default 0)
+                 (string :tag "colour code")
+                 (symbol :tag "unspecified" :value unspecified))
+  :group 'fnord-theme
+  :set (lambda (symbol value)
+         (fnord--update-custom-face symbol value 'region :foreground)))
+
+
+
+(defcustom fnord-region-highlight-background 2
+  "Background color for the region.
+If set as an int: use a fnord theme colour.
+Alternatively, use a string to choose your own colour code. Note that the string
+has to be a valid colour code like \"#A01A02\""
+  :type '(choice (integer :tag "fnord theme colour" :default 0)
+                 (string :tag "colour code")
+                 (symbol :tag "unspecified" :value unspecified))
+  :group 'fnord-theme
+  :set (lambda (symbol value)
+         (fnord--update-custom-face symbol value 'region :background)))
+
+
+(defcustom fnord-uniform-mode-lines nil
+  "Enables uniform activate- and inactive mode lines using 'nord3' as background."
+  :type 'boolean
+  :group 'fnord-theme)
+
+(setopt custom-raised-buttons t)
+
+(defvar fnord--faces
+  `(;; add a dummy face  so we can detect whether theme was loaded
+    (fnord--loaded-dummy :weight bold)
+
+    (bold :weight bold)
+    (bold-italic :weight bold :slant italic)
+    (cursor :background 4)
+    
+    (default :foreground 4 :background 0)
+    (error :foreground 11 :weight bold)
+    (escape-glyph :foreground 12)
+    (italic :slant italic)
+    (shadow :foreground 3)
+    (underline :underline t)
+    (warning :foreground 13 :weight bold)
+    (success :foreground 14)
+
+    (link :underline t :foreground 8)
+    (link-visited :underline t)
+    
+    ;; highlighting
+    (region :foreground ,(or fnord-region-highlight-foreground 'unspecified)
+            :background ,(or fnord-region-highlight-background 'unspecified))
+    (highlight :foreground 8 :background 3)
+    (hl-line :background 3)
+    
+    ;; windows and frames
+    (border :foreground 4)
+    (fringe :foreground 4 :background 0)
+    (header-line :foreground 4 :background 2)
+
+    ;; font-lock
+    (font-lock-builtin-face :foreground 9)
+    (font-lock-comment-face :foreground 16)
+    (font-lock-comment-delimiter-face :foreground 16)
+    (font-lock-constant-face :foreground 9)
+    (font-lock-doc-face :foreground 16)
+    (font-lock-function-name-face :foreground 8)
+    (font-lock-keyword-face :foreground 9)
+    (font-lock-negation-char-face :foreground 9)
+    (font-lock-number-face :foreground 15)
+    (font-lock-preprocessor-face :foreground 10 :weight bold)
+    (font-lock-reference-face :foreground 9)
+    (font-lock-regexp-grouping-backslash :foreground 13)
+    (font-lock-regexp-grouping-construct :foreground 13)
+    (font-lock-string-face :foreground 14)
+    (font-lock-type-face :foreground 7)
+    (font-lock-variable-name-face :foreground 4)
+    (font-lock-warning-face :foreground 13)
+
+    (completions-annotations :foreground 9)
+    (completions-common-part :foreground 8 :weight 'bold)
+    (completions-first-difference :foreground 11)
+    
+    (buffer-menu-buffer :foreground 4 :weight 'bold)
+
+    (button :background 0 :foreground 8 :box (:line-width 1 :color 9 :style released-button))
+    (widget-button-pressed :background 3 :foreground 7 :box (:line-width 1 :color 9 :style pressed-button))
+    (widget-documentation :foreground 4)
+    (widget-inactive :foreground 3)
+    (widget-field :background 2 :foreground 4)
+    (widget-single-line-field :inherit widget-field)
+
+    (custom-button :inherit button)
+    (custom-button-mouse :inherit widget-button-pressed)
+    (custom-button-pressed :inherit widget-button-pressed)
+    (custom-button-pressed-unraised  :background 3 :foreground 7 :box (:line-width 1 :color 9 :style flat-button))
+    (custom-button-unraised :background 0 :foreground 8 :box (:line-width 1 :color 9 :style flat-button))
+    (custom-changed :foreground 13)
+    (custom-comment :foreground fnord-comment)
+    (custom-comment-tag :foreground 7)
+    (custom-documentation :foreground 4)
+    (custom-group-tag :foreground 8 :weight bold)
+    (custom-group-tag-1 :foreground 8 :weight bold)
+    (custom-invalid :foreground 11)
+    (custom-link :inherit link)
+    (custom-modified :foreground 13)
+    (custom-rogue :foreground 12 :background 2)
+    (custom-saved :foreground 14)
+    (custom-set :foreground 8)
+    (custom-state :foreground 14)
+    (custom-themed :foreground 8 :background 2)
+    (custom-visibility :foreground 9)
+    
+    (file-name-shadow :inherit shadow)
+    (help-argument-name :foreground 8)
+
+    (info-title-4 :foreground 7 :weight bold :inherit variable-pitch)
+    (info-title-1 :height 1.2 :inherit info-title-2)
+    (info-title-2 :height 1.2 :inherit info-title-3)
+    (info-title-3 :height 1.2 :inherit info-title-4)
+    (info-menu-header :inherit info-title-3 :foreground 6)
+    (Info-quoted :foreground 6)
+    (info-menu-star :foreground 15)
+
+    ;; search/replace
+    (isearch :foreground 0 :background 8)
+    (isearch-fail :foreground 11)
+    (match :inherit :isearch)
+    (query-replace :foreground 8 :background w)
+    
+
+    (linum :foreground 3 :background 0)
+    (linum-relative-current-face :foreground 3 :background 0)
+    (line-number-current-line :foreground 6)
+    
+    (minibuffer-prompt :foreground 8 :weight bold)
+    (mm-command-output :foreground 8)
+    (mode-line :foreground 8 :background 3)
+    (mode-line-buffer-id :weight bold)
+    (mode-line-highlight :inherit highlight)
+    (mode-line-inactive :foreground 4 :background 1)
+    (next-error :inherit error)
+    (nobreak-space :foreground 3)
+    
+    (org-level-1 :foreground 7 :weight extra-bold)
+    (org-level-2 :foreground 8 :weight bold)
+    (org-level-3 :foreground 9 :weight semi-bold)
+    (org-level-4 :foreground 7 :weight normal)
+    (org-level-5 :foreground 8)
+    (org-level-6 :foreground 9)
+    (org-level-7 :foreground 7)
+    (org-level-8 :foreground 8)
+    (org-level-8 :foreground 9)
+    (org-agenda-structure :foreground 9)
+    (org-agenda-date :foreground 8 :underline nil)
+    (org-agenda-done :foreground 14)
+    (org-agenda-dimmed-todo-face :background 13)
+    (org-block :foreground 4)
+    (org-block-background :background 0)
+    (org-block-begin-line :foreground 7)
+    (org-block-end-line :foreground 7)
+    (org-checkbox :foreground 9)
+    (org-checkbox-statistics-done :foreground 14)
+    (org-checkbox-statistics-todo :foreground 13)
+    (org-code :foreground 7)
+    (org-column :background 2)
+    (org-column-title :inherit org-column :weight bold :underline t)
+    (org-date :foreground 8)
+    (org-document-info :foreground 4)
+    (org-document-info-keyword :foreground 3 :weight bold)
+    (org-document-title :foreground 8 :weight bold)
+    (org-done :foreground 14 :weight bold)
+    (org-ellipsis :foreground 3)
+    (org-footnote :foreground 8)
+    (org-formula :foreground 9)
+    (org-headline-done :foreground 10)
+    (org-hide :foreground 0 :background 0)
+    (org-link :underline t)
+    (org-scheduled :foreground 14)
+    (org-scheduled-previously :foreground 13)
+    (org-scheduled-today :foreground 8)
+    (org-special-keyword :foreground 9)
+    (org-table :foreground 9)
+    (org-todo :foreground 13 :weight bold)
+    (org-upcoming-deadline :foreground 12)
+    (org-warning :foreground 13 :weight bold)
+    (org-sexp-date :foreground 7)
+    (org-quote :inherit org-block :slant italic)
+    (org-verse :inherit org-block :slant italic)
+    (org-agenda-date-weekend :foreground 9)
+    (org-agenda-date-today :foreground 8 :weight bold)
+    (org-agenda-done :foreground 14)
+    (org-verbatim :foreground 7)
+
+    
+    (outline-1 :foreground 7 :weight bold)
+    (outline-2 :foreground 8 :inherit outline-1)
+    (outline-3 :foreground 9 :inherit outline-1)
+    (outline-4 :foreground 7 :inherit outline-1)
+    (outline-5 :foreground 8 :inherit outline-1)
+    (outline-6 :foreground 9 :inherit outline-1)
+    (outline-7 :foreground 7 :inherit outline-1)
+    (outline-8 :foreground 8 :inherit outline-1)
+    
+    (package-description :foreground 4)
+    (package-help-section-name :foreground 8 :weight bold)
+    (package-name :foreground 8)
+    (package-status-available :foreground 7)
+    (package-status-avail-obso :foreground 7 :slant italic)
+    (package-status-built-in :foreground 9)
+    (package-status-dependency :foreground 8 :slant italic)
+    (package-status-disabled :foreground 3)
+    (package-status-external :foreground 12 :slant italic)
+    (package-status-held :foreground 4 :weight bold)
+    (package-status-new :foreground 14)
+    (package-status-incompat :foreground 11)
+    (package-status-installed :foreground 7 :weight bold)
+    (package-status-unsigned :underline t)
+
+    (scroll-bar :background 3)
+    (secondary-selection :background 2)
+
+    (show-paren-match :foreground 0 :background 8)
+    (show-paren-mismatch :background 11)
+    (term :foreground 4 :background 0)
+    (term-color-black :foreground 1 :background 1)
+    (term-color-white :foreground 5 :background 5)
+    (term-color-cyan :foreground 7 :background 7)
+    (term-color-blue :foreground 8 :background 8)
+    (term-color-red :foreground 11 :background 11)
+    (term-color-yellow :foreground 13 :background 13)
+    (term-color-green :foreground 14 :background 14)
+    (term-color-magenta :foreground 15 :background 15)
+    (ansi-color-black :foreground 1 :background 1)
+    (ansi-color-white :foreground 5 :background 5)
+    (ansi-color-cyan :foreground 7 :background 7)
+    (ansi-color-blue :foreground 8 :background 8)
+    (ansi-color-red :foreground 11 :background 11)
+    (ansi-color-yellow :foreground 13 :background 13)
+    (ansi-color-green :foreground 14 :background 14)
+    (ansi-color-magenta :foreground 15 :background 15)
+    
+    (tool-bar :foreground 4 :background 3)
+    (tooltip :foreground 0 :background 4)
+    (trailing-whitespace :foreground 3)
+    (tty-menu-disabled-face :foreground 1)
+    (tty-menu-enabled-face :background 2 :foreground 4)
+    (tty-menu-selected-face :foreground 8 :underline t)
+    (undo-tree-visualizer-current-face :foreground 8)
+    (undo-tree-visualizer-default-face :foreground 4)
+    (undo-tree-visualizer-unmodified-face :foreground 4)
+    (undo-tree-visualizer-register-face :foreground 9)
+    (vc-conflict-state :foreground 12)
+    (vc-edited-state :foreground 13)
+    (vc-locally-added-state :underline t)
+    (vc-locked-state :foreground 10)
+    (vc-missing-state :foreground 11)
+    (vc-needs-update-state :foreground 12)
+    (vc-removed-state :foreground 11)
+    (vc-state-base :foreground 4)
+    (vc-up-to-date-state :foreground 8)
+    (vertical-border :foreground 2)
+    (which-func :foreground 8)
+    (whitespace-big-indent :foreground 3 :background 0)
+    (whitespace-empty :foreground 3 :background 0)
+    (whitespace-hspace :foreground 3 :background 0)
+    (whitespace-indentation :foreground 3 :background 0)
+    (whitespace-line :background 0)
+    (whitespace-newline :foreground 3 :background 0)
+    (whitespace-space :foreground 3 :background 0)
+    (whitespace-space-after-tab :foreground 3 :background 0)
+    (whitespace-space-before-tab :foreground 3 :background 0)
+    (whitespace-tab :foreground 3 :background 0)
+    (whitespace-trailing :inherit trailing-whitespace :background 15)
+
+    (window-divider :background 3)
+    (window-divider-first-pixel :background 3)
+    (window-divider-last-pixel :background 3)
+
+    (tab-bar :inherit mode-line-inactive)
+    (tab-bar-tab :inherit mode-line-highlight)
+    (tab-bar-tab-inactive :inherit tab-bar))
+    
+    
+    "The list of faces defined by the fnord theme.
+Foreground and background colours can be ints, in which
+case they will be converted to fnord theme colours.")
+
+(provide 'fnord-core)
+
+;;; fnord-core.el ends here
